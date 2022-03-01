5b7cecb98f6ed8ed44499b7593f2e374ce7e6f7c
JagratPatkar <jagratpatkar@gmail.com>
2022-03-01 06:06:13 +0530

    Update

--- src/alu.rs
+++ src/alu.rs
@@ -12,7 +12,12 @@ impl ALU{
         self.result = rf.src1 + dec.imm
     }
 
+    fn com_add(&mut self,rf : &mut RefMem){
+        self.result = rf.src1 + rf.src2
+    }
+
     pub fn compute(&mut self,dec: &mut Decoder,rf : &mut RefMem,dm : &mut DataMem){
         if dec.is_addi() { self.com_addi(dec,rf) }
+        else if dec.is_add() { self.com_add(rf) }
     }
 }

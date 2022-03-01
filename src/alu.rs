aaa25a27db843cc8cd3fa221ecb49d8f0103cc23
JagratPatkar <jagratpatkar@gmail.com>
2022-03-01 09:04:59 +0530

    Update

--- src/alu.rs
+++ src/alu.rs
@@ -1,12 +1,39 @@
 use crate::decoder::Decoder;
 use crate::dm::DataMem;
 use crate::rfm::RefMem;
+use bit::BitIndex;
 
 pub struct ALU{
     pub result: u32
 }
 
 impl ALU{
+
+    fn stlu_result(&mut self,rf : &mut RefMem) -> u32{
+        let mut t : u32 = 0x0;
+        t.set_bit(1,rf.src1 < rf.src2);
+        t
+    }
+
+    fn stliu_result(&mut self,rf : &mut RefMem,dec : &mut Decoder) -> u32{
+        let mut t : u32 = 0x0;
+        t.set_bit(1,(rf.src1 < dec.imm));
+        t
+    }
+
+    fn sext_src1(&mut self,rf : &mut RefMem) -> u64 { 
+        let mut t: u64 = 0x0;
+        t.set_bit_range(0..32,rf.src1.into());
+        if rf.src1.bit(31) { t.set_bit_range(32..64,u32::MAX.into()); }
+        else { t.set_bit_range(32..64,0u64); }
+        t
+    }
+
+    fn sra_result(&mut self,rf : &mut RefMem) -> u64
+    { self.sext_src1(rf) >> rf.src2.bit_range(0..5) }
+
+    fn srai_result(&mut self,rf : &mut RefMem,dec : &mut Decoder) -> u64
+    { self.sext_src1(rf) >> dec.imm.bit_range(0..5) }
 
     pub fn compute(&mut self,dec: &mut Decoder,rf : &mut RefMem,dm : &mut DataMem){
         if dec.is_addi() {  self.result = rf.src1.overflowing_add(dec.imm).0 }
